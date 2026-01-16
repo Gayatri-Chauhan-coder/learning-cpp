f9f181dc430bf501082c4890b2429a64f612d33b
gayatri.builds <gayatrichauhan915@gmail.com>
2026-01-16 22:38:10 +0530

    Create swap_two_digits.cpp

---
+++ swap_two_digits.cpp
@@ -0,0 +1,14 @@
+//program to swap two numbers using a function.
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a=2;
+    int b=4;
+    swap(a,b); //swapping the value of a and b
+    cout<<"the value of a:"<<a;
+    cout<<"the value of b:"<<b;
+
+    return 0;
+    }
+  
